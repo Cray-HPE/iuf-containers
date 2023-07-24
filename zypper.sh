5523fb32f34fb28ba2ac658b9d4ec989e68c4d06
Di Zou <di.zou@hpe.com>
2023-07-24 09:24:47 -0400

    try build

--- zypper.sh
+++ zypper.sh
@@ -13,6 +13,6 @@ zypper --non-interactive ar ${SLES_MIRROR}/Updates/SLE-Module-Containers/15-SP4/
 zypper --non-interactive ar --no-gpgcheck ${CSM_RPMS_HPE_STABLE}/sle-15sp2/?auth=basic CSM-SLE-15SP2
 zypper --non-interactive ar --no-gpgcheck ${CSM_RPMS_HPE_STABLE}/sle-15sp3/?auth=basic CSM-SLE-15SP3
 zypper update -y
-zypper install -y craycli loftsman manifestgen git-core jq curl kubectl
+zypper install -y craycli loftsman manifestgen git-core jq curl kubernetes-client
 zypper clean -a && zypper --non-interactive rr --all && rm -f /etc/zypp/repos.d/*
 
